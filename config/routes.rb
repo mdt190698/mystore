6f69cf9743724fa73aa6bc49b5ebda8a650d6308
mdt190698 <nameno220200@gmail.com>
2025-01-09 15:35:57 +0700

    new code 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "/products", to: "products#index"
-  resources :products
+  resources :products do
+    resources :subscribers, only: [ :create ]
+  end
   root "products#index"
-
 end
